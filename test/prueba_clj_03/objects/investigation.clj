b717357c41b71b8e944db79a3aeccd4b814d9fcc
David Kotlirevsky <david.kotlirevsky@gmail.com>
2024-05-29 10:53:40 -0300

    generate a private atribute
    	And getters and private methods
    	constructor as part of object

--- test/prueba_clj_03/objects/investigation.clj
+++ test/prueba_clj_03/objects/investigation.clj
@@ -1,27 +1,57 @@
 (ns prueba-clj-03.objects.investigation
   (:require [clojure.test :refer :all]))
-
-
-(def person {:props  {:name "David"
-                      :age 48}
-             :actions {:tell-your-name #(str "My name is " (-> % :props :name))
-                       :add-10-to-age #(+ (-> %1 :props :age) %2)}})
-
-(defn constructor [name age]
-  (let [updater (fn [persona att val] (update-in persona [:props (keyword att)] (fn [_] val)))
-        name-updated (updater person :name name)]
-    (updater name-updated :age age)))
+;Object API ==================================
+(def NOT_VALID_ATTRIBUTE "NOT_VALID_ATTRIBUTE")
+(def updater (fn [persona att val] (update-in persona [:props (keyword att)] (fn [_] val))))
+(def updater-private (fn [persona att val] (update-in persona [:private-props (keyword att)] (fn [_] (fn [] val)))))
 
 (defn exec
-  ([a-person action]
-   ((-> a-person :actions action) a-person))
-  ([a-person action parm]
-   ((-> a-person :actions action) a-person parm)))
+  ([subject action]
+   ((-> subject :actions action) subject))
+  ([subject action param]
+   ((-> subject :actions action) subject param)))
+
+(defn getter
+  [subject action]
+   ((-> subject :getters action) subject))
+; ==================================
+; ==================================
+
+; Business ==================================
+
+(defn- do_walk
+  [init step]
+  (if (> init step)
+    (format "walked %s steps" step)
+    (do
+      (println (format "walked %s step" init))
+      (do_walk (inc init) step))))
+
+(def person {:constructor (fn ([name age dni]
+                               (-> person
+                                   (updater :name name)
+                                   (updater :age age)
+                                   (updater-private :dni dni))))
+             :private-props  {:dni #(NOT_VALID_ATTRIBUTE) }
+             :props  {:name NOT_VALID_ATTRIBUTE
+                      :age NOT_VALID_ATTRIBUTE}
+             :actions {:tell-your-name #(str "My name is " (-> % :props :name))
+                       :add-10-to-age #(+ (-> %1 :props :age) %2)
+                       :walk #(do_walk 1 %2)}
+             ; optional
+             :getters {:dni #((-> %1 :private-props :dni))}})
+
+; ==================================
+; ==================================
+
+; Tests ==================================
 
 (deftest person-test
-  (let [charly (constructor "carlos" 73)]
+  (let [charly ((-> person :constructor) "carlos" 73 "21912691")]
     (is (= (-> charly :props :name) "carlos"))
     (is (= (-> charly :props :age) 73))
-    (is (= ((-> charly :actions :tell-your-name) charly) "My name is carlos"))
     (is (= (exec charly :tell-your-name) "My name is carlos"))
-    (is (= (exec charly :add-10-to-age 10) 83))))
+    (is (= (exec charly :add-10-to-age 10) 83))
+    (is (= (exec charly :walk 10) "walked 10 steps"))
+    (is (= (getter charly :dni) "21912691"))
+    ))
